85d7b4ba46be2570ea6233414ca6cef7eb792f58
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-09-22 23:05:28 +0200

    Add Auth Window

--- src/main/java/Testing/Main.java
+++ src/main/java/Testing/Main.java
@@ -12,7 +12,11 @@ public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
 
         if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-            Desktop.getDesktop().browse(new URI("http://www.instagram.com"));
+            Desktop.getDesktop().browse(new URI("https://api.instagram.com/oauth/authorize" +
+                    "  ?client_id=594821241888095" +
+                    "  &redirect_uri=https://insta-test-maalti.herokuapp.com/auth/" +
+                    "  &scope=user_profile,user_media" +
+                    "  &response_type=code"));
         }
 
         String urlStr = "https://insta-test-maalti.herokuapp.com/auth/?code=AQDSEvAP0_x2ygPdafU3i-XK_1ZRCZYlwdY93sZLHDTh0Mr7RW2grpv-N5vof-1dNFUyOsPNXDEFyYWg8LDaIoKDP_zSdGIX9x2n2PQ3ObOgH28NIk8MrdfehsnVLwJcscCly9Oreu1R9bKDw1CfWukHaA6eLCYJbf5TIT1A32WxppyLfjJHLu-oHqljZCIXipQvTelxwvsCt9zECCfIBmsN6i6WQewBL48zD8j-dxLGZQ#_";
