00bc83c31a255e139d32e78b16bddb02fe5cf0a5
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-09-22 17:04:36 +0200

    Test Build

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://www.instagram.com/revilodesign_de/channel/?__a=1";
+
+
+
+    }
+
+    @GetMapping
+
+}
