6e37e0a6217ca778f8da39dcbf766227bb4dac76
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-09-22 23:00:10 +0200

    Initital Api Test

---
+++ src/main/java/Testing/Main.java
@@ -0,0 +1,24 @@
+package Testing;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(new URI("http://www.instagram.com"));
+        }
+
+        String urlStr = "https://insta-test-maalti.herokuapp.com/auth/?code=AQDSEvAP0_x2ygPdafU3i-XK_1ZRCZYlwdY93sZLHDTh0Mr7RW2grpv-N5vof-1dNFUyOsPNXDEFyYWg8LDaIoKDP_zSdGIX9x2n2PQ3ObOgH28NIk8MrdfehsnVLwJcscCly9Oreu1R9bKDw1CfWukHaA6eLCYJbf5TIT1A32WxppyLfjJHLu-oHqljZCIXipQvTelxwvsCt9zECCfIBmsN6i6WQewBL48zD8j-dxLGZQ#_";
+        URL url = new URL(urlStr);
+        URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+        System.out.println((url.getQuery()));
+        url = uri.toURL();
+    }
+}
---
+++ src/main/java/Testing/Mapping.java
@@ -0,0 +1,12 @@
+package Testing;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/v1.0")
+public class Mapping {
+
+}
