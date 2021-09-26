3aac49d599a9d5c27eef778d97ac62e40b6a0d5a
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-09-26 21:08:02 +0200

    First Online Test

--- src/main/java/Testing/ApiExample.java
+++ src/main/java/Testing/ApiExample.java
@@ -1,10 +1,6 @@
 package Testing;
 
-/**
- * Sample Java code for youtube.channels.list
- * See instructions for running these code samples locally:
- * https://developers.google.com/explorer-help/guides/code_samples#java
- */
+
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
@@ -21,17 +17,11 @@ import java.security.GeneralSecurityException;
 
 
 public class ApiExample {
-    private static final String DEVELOPER_KEY = "AIzaSyCTrsSbAW-oOXcaQG7KUR2zuImmjaLVi9o";
 
-    private static final String APPLICATION_NAME = "API code samples";
+    private static final String DEVELOPER_KEY = "AIzaSyCTrsSbAW-oOXcaQG7KUR2zuImmjaLVi9o";
+    private static final String APPLICATION_NAME = "Maalti";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    /**
-     * Build and return an authorized API client service.
-     *
-     * @return an authorized API client service
-     * @throws GeneralSecurityException, IOException
-     */
     public static YouTube getService() throws GeneralSecurityException, IOException {
         final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
         return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
@@ -39,24 +29,21 @@ public class ApiExample {
                 .build();
     }
 
-    /**
-     * Call function to create API service object. Define and
-     * execute API request. Print API response.
-     *
-     * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
-     */
-    public static void main(String[] args) throws GeneralSecurityException, IOException, GoogleJsonResponseException {
-        YouTube youtubeService = getService();
+
+
+    public static Youtuber doThing() throws GeneralSecurityException, IOException {
+        YouTube youtubeService = ApiExample.getService();
         YouTube.Channels.List request = youtubeService.channels()
                 .list("snippet,contentDetails,statistics");
         ChannelListResponse response = request.setKey(DEVELOPER_KEY)
                 .setForUsername("Gronkh")
                 .execute();
-        System.out.println(response.getItems().get(0).getSnippet().getTitle());
+
+
         ChannelSnippet snippet = response.getItems().get(0).getSnippet();
         ChannelStatistics statistics = response.getItems().get(0).getStatistics();
 
         Youtuber abc = new Youtuber(snippet.getTitle(), snippet.getDescription(),statistics.getSubscriberCount());
-
+        return abc;
     }
 }
--- src/main/java/Testing/Mapping.java
+++ src/main/java/Testing/Mapping.java
@@ -3,15 +3,17 @@ package Testing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1.0")
 public class Mapping {
 
     @GetMapping("/getYoutuber")
-    public Youtuber getTasks(@RequestParam(value = "YouTuber", defaultValue = "Student") String Youtuber) {
-
-    return null;
+    public Youtuber getYoutuber() throws GeneralSecurityException, IOException {
+    return ApiExample.doThing();
     }
 
 
