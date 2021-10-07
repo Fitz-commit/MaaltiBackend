c8fdda5b39ed4b7fc49977096cea06c9bddd52fe
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-10-07 20:55:03 +0200

    DemoApp rework

--- src/main/java/Testing/DemoApplication.java
+++ src/main/java/Testing/DemoApplication.java
@@ -1,11 +1,7 @@
-package com;
+package Testing;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @SpringBootApplication
