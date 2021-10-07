4574979d068d2252ef39f43e5d6c2a9706662fca
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-10-07 18:20:23 +0200

    Testing

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import Testing.YTAPICall;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+
+        YTAPICall.channelListUsername("Gronkh");
+
+    }
+}
