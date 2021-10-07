9f28640d2a72dc44cae7bd07e816b4d34fc67342
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-10-07 20:08:22 +0200

    DemoApp rework

--- src/main/java/Testing/Mapping.java
+++ src/main/java/Testing/Mapping.java
@@ -17,7 +17,7 @@ public class Mapping {
     //Abklären was wir von der API brauchen.
     //Theoretisch alles möglich mit API KEY?
 
-    @GetMapping("/getYotuber")
+    @GetMapping("/getYoutuber")
     public Youtuber getYoutuber( String Youtuber) throws GeneralSecurityException, IOException {
 
         return YTAPICall.channelListUsername(Youtuber);
