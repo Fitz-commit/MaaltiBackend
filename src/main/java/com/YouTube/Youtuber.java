d74f0e6843700b516a0c3d71fb4ab1334e7eaf63
Federolf <Tim.Federolf@de.ebmpapst.com>
2021-10-12 10:21:54 +0200

    Youtuber Klasse wird nun komplett gefüllt

--- src/main/java/com/YouTube/Youtuber.java
+++ src/main/java/com/YouTube/Youtuber.java
@@ -22,14 +22,14 @@ public class Youtuber {
     private List<String> topics;
     private VideoListResponse videos;
     private int itemcount ;
-    private BigInteger totalviews;
-    private BigInteger totalcomments;
-    private BigInteger totallikes;
-    private BigInteger totaldislikes;
-    private BigInteger averageviews;
-    private BigInteger averagecomments;
-    private BigInteger averagelikes;
-    private BigInteger averagedislikes;
+    private BigInteger totalviews = BigInteger.valueOf(0);
+    private BigInteger totalcomments = BigInteger.valueOf(0);
+    private BigInteger totallikes = BigInteger.valueOf(0);
+    private BigInteger totaldislikes = BigInteger.valueOf(0);
+    private BigInteger averageviews = BigInteger.valueOf(0);
+    private BigInteger averagecomments = BigInteger.valueOf(0);
+    private BigInteger averagelikes= BigInteger.valueOf(0);
+    private BigInteger averagedislikes= BigInteger.valueOf(0);
 
 
     public Youtuber(String name, String profilbild, String creatorid) {
@@ -53,14 +53,14 @@ public class Youtuber {
         this.topics = topics;
     }
 
-    public void getKennzahlenBasis(){
+    public void kennzahlenBasis(){
         int counter=0;
 
         for(ListIterator<com.google.api.services.youtube.model.Video> VideoIterator = videos.getItems().listIterator(); VideoIterator.hasNext(); ){
-            totalviews.add(videos.getItems().get(counter).getStatistics().getViewCount());
-            totalcomments.add(videos.getItems().get(counter).getStatistics().getCommentCount());
-            totallikes.add(videos.getItems().get(counter).getStatistics().getLikeCount());
-            totaldislikes.add(videos.getItems().get(counter).getStatistics().getDislikeCount());
+            totalviews =totalviews.add(videos.getItems().get(counter).getStatistics().getViewCount());
+            totalcomments =totalcomments.add(videos.getItems().get(counter).getStatistics().getCommentCount());
+            totallikes= totallikes.add(videos.getItems().get(counter).getStatistics().getLikeCount());
+            totaldislikes =totaldislikes.add(videos.getItems().get(counter).getStatistics().getDislikeCount());
             counter++;
             VideoIterator.next();
         }
@@ -68,10 +68,10 @@ public class Youtuber {
     }
 
     public void berechneKennzahlen(){
-        averageviews.add(totalviews.divide(BigInteger.valueOf(itemcount)));
-        averagecomments.add(totalcomments.divide(BigInteger.valueOf((itemcount))));
-        averagelikes.add(totallikes.divide(BigInteger.valueOf(itemcount)));
-        averagedislikes.add(totaldislikes.divide(BigInteger.valueOf((itemcount))));
+        averageviews = totalviews.divide(BigInteger.valueOf(itemcount));
+        averagecomments = totalcomments.divide(BigInteger.valueOf((itemcount)));
+        averagelikes =totallikes.divide(BigInteger.valueOf(itemcount));
+        averagedislikes =totaldislikes.divide(BigInteger.valueOf((itemcount)));
     }
 
 
@@ -121,11 +121,55 @@ public class Youtuber {
         return topics;
     }
 
-    public void setVideos(VideoListResponse videos) {
-        this.videos = videos;
+    public String getCountry() {
+        return country;
     }
 
     public VideoListResponse getVideos() {
         return videos;
     }
+
+    public int getItemcount() {
+        return itemcount;
+    }
+
+    public BigInteger getTotalviews() {
+        return totalviews;
+    }
+
+    public BigInteger getTotalcomments() {
+        return totalcomments;
+    }
+
+    public BigInteger getTotallikes() {
+        return totallikes;
+    }
+
+    public BigInteger getTotaldislikes() {
+        return totaldislikes;
+    }
+
+    public BigInteger getAverageviews() {
+        return averageviews;
+    }
+
+    public BigInteger getAveragecomments() {
+        return averagecomments;
+    }
+
+    public BigInteger getAveragelikes() {
+        return averagelikes;
+    }
+
+    public BigInteger getAveragedislikes() {
+        return averagedislikes;
+    }
+
+    public void setVideos(VideoListResponse videos) {
+        this.videos = videos;
+        itemcount = this.videos.getItems().size();
+        kennzahlenBasis();
+        berechneKennzahlen();
+    }
+
 }
